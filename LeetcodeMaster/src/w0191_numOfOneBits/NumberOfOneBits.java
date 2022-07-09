b9b6182d3c82b33938033b13ea110ce8a31c272a
yiwen <yiwenhaha@gmail.com>
2022-07-09 22:59:31 +0800

    3/4 eastern lake

---
+++ LeetcodeMaster/src/w0191_numOfOneBits/NumberOfOneBits.java
@@ -0,0 +1,11 @@
+package w0191_numOfOneBits;
+
+public class NumberOfOneBits {
+
+	// you need to demostrate more of algorithmic thinking *duh*
+	
+	// you need to treat n as an unsigned value
+	public int hammingWeight(int n) {
+		return Integer.bitCount(n);
+	}
+}
